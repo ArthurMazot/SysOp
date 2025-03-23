885274e7176767c72d80f5de67c4189ed34a762a
ArthurMazot <arthurmazot@hotmail.com>
2025-03-23 19:45:20 -0300

    Add files via upload

--- SysOp/GP.java
+++ SysOp/GP.java
@@ -3,28 +3,35 @@ package SysOp;
 import SysOp.Sistema.Word;
 
 public class GP {
-    PCB[] processos;
-    Word[] mem;
+    private int tamPag;
+    private PCB[] processosProntos;
+    private Word[] mem;
     private GM gm;
 
-    public GP(Word[] m, int tamPag){
-        processos = new PCB[m.length/tamPag]; //Não sei o tamanho
-        for(int i = 0; i < processos.length; i++)
-            processos[i] = null;
-        gm = new GM(m.length, tamPag);
+    public GP(Word[] m, int tamP){
+        processosProntos = new PCB[m.length/tamP]; //Não sei o tamanho
+        tamPag = tamP;
+        for(int i = 0; i < processosProntos.length; i++)
+            processosProntos[i] = null;
+        gm = new GM(m.length, tamP);
         mem = m;}
 
     boolean criaProcesso(Word[] prog){
         int[] tabPag = null;
-        if(!gm.aloca(prog.length, tabPag)) return false; //verifica se tem espaço na memoria
-        for(int i = 0; i < processos.length; i++) //coloca na lista de processos
-            if(processos[i] == null){
-                processos[i] = new PCB(i, tabPag);
+        if(!gm.aloca(prog.length, tabPag)) //verifica se tem espaço na memoria
+            return false;
+
+        for(int i = 0; i < tamPag; i++) //colaca o programa nas paginas alocadas
+            for(int j = 0; j < tamPag; j++)
+                mem[tabPag[i]*tamPag + j] = prog[i*tamPag + j]; //se tiver reclamando que tabPag pode ser null, não vai ser pq no gm.aloca se não encontrar espaço na memoria ele entra no if e retonra falso;
+
+        for(int i = 0; i < processosProntos.length; i++) //coloca na lista de processos
+            if(processosProntos[i] == null){
+                processosProntos[i] = new PCB(i, tabPag);
                 break;}
         return true;}
 
     void desalocaProcesso(int id){
-        gm.desaloca(processos[id].tabPag);
-        processos[id] = null;  //aparentemente não existe um delete
-    }
+        gm.desaloca(processosProntos[id].tabPag);
+        processosProntos[id] = null;}  //aparentemente não existe um delete
 }
--- SysOp/PCB.java
+++ SysOp/PCB.java
@@ -3,8 +3,12 @@ package SysOp;
 public class PCB {
     public int id;
     public int[] tabPag;
+    public boolean rodando;
+    public int pc;
 
     public PCB(int i, int[] tab){
         id = i;
-        tabPag = tab;}
+        tabPag = tab;
+        rodando = false;
+        pc = 0;}
 }
